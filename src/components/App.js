1cd3b70ec893487a5b2da1e084d4984493367a36
AlexVotry <alexvotry@gmail.com>
2019-04-05 10:04:44 -0700

    added bootstrap cdn

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import Congrats from './Congrats';
+import GuessedWords from './GuessedWords';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="container">
+      <h1>Jotto</h1>
+        <Congrats success={true} />
+        <GuessedWords guessedWords={[{guessedWord: 'train', letterMatchCount: 3}]} />
       </div>
     );
   }
